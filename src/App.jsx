83ee4fd42b1fd21a8f72e51315dddb8a7c65ed3f
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-02-11 23:01:04 +0330

    initialize react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { BrowserRouter } from "react-router-dom";
 import "./styles/fonts.css";
 import Router from "./router/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        retry: 1,
+        staleTime: 60 * 1000,
+      },
+    },
+  });
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
