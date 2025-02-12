2cb61004385d1842ce0eedd8d4119ba09043d9fb
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-02-12 22:45:32 +0330

    set header authorization in interceptors to avoid more code

--- configs/api.js
+++ configs/api.js
@@ -1,10 +1,20 @@
 import axios from "axios";
+import { getCookie } from "./cookies";
 
 const api = axios.create({
   baseURL: "http://localhost:3400/",
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+api.interceptors.request.use((req) => {
+  const accessToken = getCookie();
+  if (accessToken) {
+    req.headers.Authorization = `bearer ${accessToken}`;
+  }
+
+  return req;
 });
 
 export default api;
