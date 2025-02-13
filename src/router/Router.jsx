56eb1c59ab70b673e11044365c8764db0deb0ee6
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-02-13 21:09:36 +0330

    Redirecting and restricting access like the admin panel

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import DashboardPage from "../pages/DashboardPage";
 import AdminPage from "../pages/AdminPage";
@@ -18,13 +18,32 @@ function Router() {
 
   console.log(data);
 
+  //09189990099 : ADMIN phone
+
+  if(isLoading) return <p>isLoading...</p>
+
   return (
     <>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route
+          path="/dashboard"
+          element={data ? <DashboardPage /> : <Navigate to="/auth" />}
+        />
+        <Route
+          path="/auth"
+          element={data ? <Navigate to="/dashboard" /> : <AuthPage />}
+        />
+        <Route
+          path="/admin"
+          element={
+            data && data.data.role === "ADMIN" ? (
+              <AdminPage />
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
