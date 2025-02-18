97e6f854c6f7629c45cebc251c9afedf3abb4ff6
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-02-19 00:34:47 +0330

    postList tailwindcss style

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -15,16 +15,22 @@ function PostList() {
         <p>is loading ...</p>
       ) : (
         <>
-          <h3>آگهی های شما</h3>
+          <h3
+            className="mt-[60px] mb-[30px] border-b-[3px] border-solid border-[#a62626] 
+          w-fit pb-[5px]"
+          >
+            آگهی های شما
+          </h3>
           {data.data.posts.map((post) => (
-            <div key={post._id}>
+            <div key={post._id} className="flex items-center border-[2px] border-solid 
+            border-[#eaeaea] rounded-[5px] mt-[10px] mb-[10px] p-[5px]  ">
               {/* <img src={`http://localhost:3400/${post.images[0]}`} /> */}
-              <img src="download.jpg" />
-              <div>
-                <p>{post?.options?.title}</p>
-                <span>{post?.options?.content}</span>
+              <img src="download.jpg" className="w-[100px] h-[70px] rounded-[3px] ml-[30px]"/>
+              <div className="w-screen">
+                <p className="text-[.9rem]">{post?.options?.title}</p>
+                <span className="text-[.8rem] text-gray-500">{post?.options?.content}</span>
               </div>
-              <div>
+              <div className="w-[150px] text-center">
                 <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
                 <p>{post.amount.toLocaleString()} تومان</p>
               </div>
