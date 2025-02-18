438a5f4b9d3ff2863200610177eae77928779c69
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-02-19 00:14:09 +0330

    Show user ads in the panel

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,15 +1,39 @@
-import {useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import api from "../../../configs/api";
 
 function PostList() {
-  const { data, isLoading } = useQuery({
+  const { data, isFetching } = useQuery({
     queryKey: ["my-post-list"],
     queryFn: () => api.get("post/my"),
   });
 
-  console.log(data)
-  return <div>PostList</div>;
+  console.log(data);
+  return (
+    <div>
+      {isFetching ? (
+        <p>is loading ...</p>
+      ) : (
+        <>
+          <h3>آگهی های شما</h3>
+          {data.data.posts.map((post) => (
+            <div key={post._id}>
+              {/* <img src={`http://localhost:3400/${post.images[0]}`} /> */}
+              <img src="download.jpg" />
+              <div>
+                <p>{post?.options?.title}</p>
+                <span>{post?.options?.content}</span>
+              </div>
+              <div>
+                <p>{post.createdAt}</p>
+                <p>{post.amount} تومان</p>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default PostList;
