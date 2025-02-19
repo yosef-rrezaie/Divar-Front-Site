236002447b3c5200be4663bfe2e8e8d6017af3cf
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-02-19 16:46:07 +0330

    complete sidebar and styled with tailwindcss

--- src/components/templates/SideBar.jsx
+++ src/components/templates/SideBar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import api from "../../../configs/api";
 
 function SideBar() {
+  const { data } = useQuery({
+    queryKey: ["get-categories"],
+    queryFn: () => api.get("category"),
+  });
+
+  console.log(data);
   return (
-    <div>SideBar</div>
-  )
+    <div className="mt-[30px] w-[200px]">
+      <h4>دسته ها</h4>
+      <ul>
+        {data?.data?.map((category) => (
+          <li key={category._id} className="flex mt-[20px] mb-[20px]">
+            <img src={`${category.icon}.svg`} />
+            <p className="font-extralight mr-[10px] text-gray-600">{category.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default SideBar
+export default SideBar;
